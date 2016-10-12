2e16c1379354b406497991e6e2f10b80ba3a8401
anas.t@almacom.co.th <anas.t@almacom.co.th>
2016-10-12 10:56:31 +0700

    fisrt commit : setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="netforce_auth_ldap",
+    version="3.1.0",
+    description="Ldap Module",
+    install_requires=[
+        "ldap3>=1.4.0"
+    ],
+
+)
